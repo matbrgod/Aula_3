15cee677de58a2d3b94a6e0e7039312900d74ed4
matbrgod <mcosta.barroso@gmail.com>
2025-08-20 10:25:25 -0300

    Adicionei tiles em que o player morre e pulo preciso

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -1,43 +1,57 @@
 using UnityEngine;
-using UnityEngine.SceneManagement;
-using System.Collections;
-using System.Collections.Generic;
-using Unity.Hierarchy;
 
 public class PlayerMove : MonoBehaviour
 {
+    public float speed = 10f; // Speed of player movement
+    Vector2 movement; // Movement vector
+    public float jumpForce = 300f; // Force applied when jumping
+    Rigidbody2D rb;
+
+    bool canJump = true; // Flag to check if the player can jump
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    //Global variables can be used in any method in this class
-
-    public float moveSpeed = 10f;
-    public Rigidbody2D rb;
-    Vector2 moveDirection;
-    public float jumpForce = 500f;
-    
-
-
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-       
     }
 
     // Update is called once per frame
     void Update()
     {
-        moveDirection = new Vector2(Input.GetAxis("Horizontal"),0);
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            rb.AddForce(new Vector2(0, jumpForce));
+        movement = new Vector2(Input.GetAxis("Horizontal"), 0);
 
+        // Allow jumping only when the player is on the ground
+        if (Input.GetKeyDown(KeyCode.Space) && canJump)
+        {
+            rb.AddForce(new Vector2(0, jumpForce)); // Apply jump force
+            canJump = false; // Set canJump to false to prevent double jumping
         }
 
     }
+    // FixedUpdate is called at a fixed interval and is independent of frame rate. Put physics code here.
     void FixedUpdate()
     {
-        // Movement
-        rb.AddForce(moveDirection * moveSpeed);
-        
+        rb.AddForce(movement * speed);
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        // Check if the player collides with the ground or any other object
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            Destroy(gameObject); // Destroy the player object if it collides with an enemy
+
+        }
+
+        // Check if the player is on the ground
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            canJump = true; // Allow jumping again when the player is on the ground
+        }
+    }
+
+    void OnCollisionStay2D(Collision2D collision)
+    {
 
 
     }
