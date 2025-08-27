f1b8aa5a37596c7e41ae4386ea1a6b8b5fbb9640
matbrgod <mcosta.barroso@gmail.com>
2025-08-27 10:27:07 -0300

    Adicionei animações

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -6,19 +6,34 @@ public class PlayerMove : MonoBehaviour
     Vector2 movement; // Movement vector
     public float jumpForce = 300f; // Force applied when jumping
     Rigidbody2D rb;
+    Animator animator;
+    SpriteRenderer spriteRenderer;
 
     bool canJump = true; // Flag to check if the player can jump
+
+    float distanceToGround;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();        
     }
 
     // Update is called once per frame
     void Update()
     {
         movement = new Vector2(Input.GetAxis("Horizontal"), 0);
+        if (movement.x < 0)
+        {
+            spriteRenderer.flipX = false;
+        }
+        if (movement.x > 0)
+        {
+            spriteRenderer.flipX = true;
+        }
+
 
         // Allow jumping only when the player is on the ground
         if (Input.GetKeyDown(KeyCode.Space) && canJump)
@@ -26,12 +41,25 @@ public class PlayerMove : MonoBehaviour
             rb.AddForce(new Vector2(0, jumpForce)); // Apply jump force
             canJump = false; // Set canJump to false to prevent double jumping
         }
+        animator.SetBool("Pulando", !canJump);
 
     }
     // FixedUpdate is called at a fixed interval and is independent of frame rate. Put physics code here.
     void FixedUpdate()
     {
-        rb.AddForce(movement * speed);
+        // rb.AddForce(movement * speed);
+        Vector3 newMovement = new Vector3(movement.x * speed, rb.linearVelocity.y, 0);
+        rb.linearVelocity = newMovement;
+        animator.SetFloat("Velocidade.x", Mathf.Abs(movement.x));
+
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, 5f);
+        if(hit.collider != null)
+        {
+            distanceToGround = hit.distance;
+            animator.SetFloat("DistaciaChao", distanceToGround);
+            Debug.DrawLine(transform.position, hit.point, Color.red);
+        }
+
     }
 
     void OnCollisionEnter2D(Collision2D collision)
